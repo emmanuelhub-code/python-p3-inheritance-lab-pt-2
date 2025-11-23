6b5e3ee8336b97de61d5505611a71a0ccb8d3897
emmanuel <emmanuelhongo5@gmail.com>
2025-11-23 15:07:45 +0300

    lab work

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,25 @@
+# student.py
+
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
+
+
+# chatty_student.py
+
+from student import Student
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        # call the parent version first
+        super().hello()
+        # then add the chatty part
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        # call the parent method 10 times
+        for _ in range(10):
+            super().raise_hand()
